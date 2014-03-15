27e2a798eaf8b656c51be5b8b06a50140bdb70f0
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2014-03-15 08:20:21 -0400

    more examples in typeinfo_readable

--- cpp/typeinfo_readable/typeinfo_readable.cpp
+++ cpp/typeinfo_readable/typeinfo_readable.cpp
@@ -7,18 +7,29 @@
 #include <memory>
 #include <cstdlib>
 
-std::string demangle(const char* mangled)
+struct Widget 
+{};
+
+std::string demangle (const char* mangled)
 {
-      int status;
-      std::unique_ptr<char[], void (*)(void*)> result(
-        abi::__cxa_demangle(mangled, 0, 0, &status), std::free);
-      return result.get() ? std::string(result.get()) : "error occurred";
+  int status;
+  std::unique_ptr<char[], void (*)(void*)> result(
+						  abi::__cxa_demangle(mangled, 0, 0, &status), std::free);
+  return result.get() ? std::string(result.get()) : "error occurred";
 }
 
 template<class T>
-void foo(T t) { std::cout << demangle(typeid(t).name()) << std::endl; }
+void foo_name (T t) { std::cout << demangle(typeid(t).name()) << std::endl; }
+
+template<class T>
+void foo_typeinfo (T t) { std::cout << typeid(t).name() << std::endl; }
 
 int main() {
-    foo("f");            //char const*
-    foo(std::string());  //std::string
+  
+  foo_name ("f");            //char const*
+  foo_name (std::string());  //std::string
+  foo_name (Widget ());      //Widget
+  foo_typeinfo ("f");            
+  foo_typeinfo (std::string());  
+  foo_typeinfo (Widget ());      
 }
