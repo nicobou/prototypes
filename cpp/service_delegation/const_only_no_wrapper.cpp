852108331c4e200547166faacb6037937b1e2f66
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2014-09-30 23:24:13 -0400

    using ConstMethodsInvoker for delegation

--- cpp/service_delegation/const_only_no_wrapper.cpp
+++ cpp/service_delegation/const_only_no_wrapper.cpp
@@ -19,39 +19,41 @@ struct Service {
   }
 };
 
-class ServiceOwner {
- public:
+template<typename S>
+class ConstMethodsInvoker {
+public:
   // exposing public service const methods
   template<typename R, typename ...Ts>
-  R invoke_service(R(Service::*function)(Ts...) const, Ts ...args)
+  R invoke_service(R(S::*function)(Ts...) const, Ts ...args)
   {
-    std::function<R(Service*, Ts...)> fun = function;
-    return std::bind(fun, &serv, args...)();
+    std::function<R(S *, Ts...)> fun = function;
+    return std::bind(fun, get_service_instance(), args...)();
   }
 
   template<typename R, typename ...Ts>
-  R invoke_service(R(Service::*function)(Ts...), Ts ...args)
+  R invoke_service(R(S::*function)(Ts...), Ts ...args)
   {
     // disable invokation of non const members
     static_assert(std::is_const<decltype(function)>::value,
                   "invoke_service requires const methods");
     return (*new R);  // for syntax only since assert should always fail
-
-    // // or the following if non-const methods are allowed 
-    // std::function<R(Service*, Ts...)> fun = function;
-    // return std::bind(fun, &serv, args...) ();
   }
 
+private:
+  virtual S *get_service_instance() = 0;
+};
+
+class ServiceOwner: public ConstMethodsInvoker<Service> {
+  // no wraper
  private:
-  Service serv{};
+  Service serv_{};  // RAII member
+  Service *get_service_instance() final {return &serv_;}
 };
 
 int main() {
-
-  {  // play with service owner
     ServiceOwner owner;
     owner.invoke_service(&Service::identity_const, 6);
     owner.invoke_service(&Service::do_something_const, 5, 8.456);
+    // will fail with static assert
     // owner.invoke_service(&Service::identity_no_const, 5);
-  }
 }
