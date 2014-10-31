0fb7b2ae7d208b8a6cb5996484c1e428a8b5c9cf
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2014-10-31 16:38:05 -0400

    cmi

--- cpp/service_delegation/const_only_no_wrapper.cpp
+++ cpp/service_delegation/const_only_no_wrapper.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <functional>
+#include <string>
 
 struct Service {
   int identity_const(int i) const {
     std::cout << i << " (const)" << std::endl;
     return i;
   }
-  
+
+  std::string identity_str_const(const std::string &s) const {
+    std::cout << s << " (const)" << std::endl;
+    return s;
+  }
+
   double do_something_const(int a, double b) const {
     double res = a + b + 0.5;
     std::cout << res << std::endl;
@@ -17,43 +23,73 @@ struct Service {
     std::cout << i << " (no const)" << std::endl;
     return i;
   }
-};
 
-template<typename S>
-class ConstMethodsInvoker {
-public:
-  // exposing public service const methods
-  template<typename R, typename ...Ts>
-  R invoke_service(R(S::*function)(Ts...) const, Ts ...args)
-  {
-    std::function<R(S *, Ts...)> fun = function;
-    return std::bind(fun, get_service_instance(), args...)();
+  int overloaded_const (int i) const {
+    std::cout << i << " (const)" << std::endl;
+    return i;
   }
 
-  template<typename R, typename ...Ts>
-  R invoke_service(R(S::*function)(Ts...), Ts ...args)
+  double overloaded_const (double i) const {
+    std::cout << i << " const)" << std::endl;
+    return i;
+  }
+
+
+};
+
+template<typename T>
+class ConstMethodsInvoker {
+public:
+  // exposing T const methods accessible by T instance owner
+  template<typename R,       // return type
+           typename ...DTs,  // Defined arguments types
+           typename ...ATs>  // Arguments types
+  R cmi_invoke(R(T::*function)(DTs...) const, ATs ...args)
   {
-    // disable invokation of non const members
+    std::function<R(T *, DTs...)> fun = function;
+    return std::bind(std::move(fun),
+                     cmi_get(),
+                     std::forward<ATs>(args)...)();
+  }
+
+  // disable invokation of non const
+  template<typename R, typename ...DTs, typename ...ATs>
+  R cmi_invoke(R(T::*function)(DTs...), ATs ...args)
+  {
     static_assert(std::is_const<decltype(function)>::value,
-                  "invoke_service requires const methods");
+                  "ConstMethodsInvoker requires const methods only");
     return (*new R);  // for syntax only since assert should always fail
   }
 
 private:
-  virtual S *get_service_instance() = 0;
+  // require child to pass instance
+  virtual T *cmi_get() = 0;
 };
+
 
 class ServiceOwner: public ConstMethodsInvoker<Service> {
   // no wraper
  private:
   Service serv_{};  // RAII member
-  Service *get_service_instance() final {return &serv_;}
+  Service *cmi_get() final {return &serv_;}
 };
 
 int main() {
     ServiceOwner owner;
-    owner.invoke_service(&Service::identity_const, 6);
-    owner.invoke_service(&Service::do_something_const, 5, 8.456);
+    owner.cmi_invoke(&Service::identity_const, 6);
+    owner.cmi_invoke(&Service::do_something_const, 5, 8.456);
+    owner.cmi_invoke(&Service::identity_str_const, "str");
+    {
+      int (Service::*fun)(int) const = &Service::overloaded_const;
+      owner.cmi_invoke(fun, 9);
+    }
+    {
+      double (Service::*fun)(double) const = &Service::overloaded_const;
+      owner.cmi_invoke(fun, 10.5);
+    }
+    owner.cmi_invoke(
+        (double(Service::*)(double) const)&Service::overloaded_const,
+        11.876);
     // will fail with static assert
-    // owner.invoke_service(&Service::identity_no_const, 5);
+    // owner.cmi_invoke(&Service::identity_no_const, 5);
 }
